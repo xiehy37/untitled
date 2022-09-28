02b2d97907c7e1e702bc2e87b19d7d6c10dc1b81
xiehy37 <xiehy37@mail2.sysu.edu.cn>
2022-09-28 17:51:57 +0800

    Initial commit

---
+++ a.py
@@ -0,0 +1,7 @@
+import torch
+
+print(torch.cuda.is_available())
+print(torch.backends.cudnn.is_available())
+print(torch.cuda_version)
+print(torch.backends.cudnn.version())
+print(torch.__version__)
